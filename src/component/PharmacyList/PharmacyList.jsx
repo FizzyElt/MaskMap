53a50e9fbc27e57855cee67efb90910b6b958e95
Eddie Hsieh <zxcx98738@gmail.com>
2020-04-17 09:46:23 +0800

    新增功能：縣市顯示一次藥局列表，里的時候會在顯示一次

--- src/component/PharmacyList/PharmacyList.jsx
+++ src/component/PharmacyList/PharmacyList.jsx
@@ -33,14 +33,21 @@ const PharamcyItem = ({ item }) => {
     </li>)
 }
 
-const PharmacyList = ({ data, county, town,cunli }) => {
-    const list = useMemo(() => county && town //&& cunli 
-    ? data.filter(({ properties }) => {
-        return properties.county === county && properties.town === town //&& properties.cunli === cunli
-      }) : [], [town,data,county])
+const PharmacyList = ({ data, county, town, cunli }) => {
+    const list = useMemo(() => county && town && cunli
+        ? data.filter(({ properties }) => {
+            return properties.county === county && properties.town === town && properties.cunli === cunli
+        })
+          : county && town  ? data.filter(({properties})=>{
+            return  properties.county === county && properties.town === town
+          }) 
+
+        : [], [cunli, data, town, county]
+    )
     return (
         <ul className="pharmacy-list">
             {list.map(item => (<PharamcyItem item={item} key={item.properties.id} />))}
+            
         </ul>
     );
 }
