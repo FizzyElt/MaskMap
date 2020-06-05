1176a6aaf2d6db6c4e464e0a89004b85981cf345
FizzyElt <a0979150560@gmail.com>
2020-06-05 16:07:16 +0800

    eslint 導入及修改程式碼

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports={
+  "env":{
+    "browser":true,
+    "es6":true
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "plugins": ["react-hooks"],
+  "parserOptions":{
+    "sourceType":"module"
+  },
+  "parser": "babel-eslint",
+}
--- src/component/ToGoogleMapButton/ToGoogleMapButton.jsx
+++ src/component/ToGoogleMapButton/ToGoogleMapButton.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 import './ToGoogleMapButton.scss'
 
 class ToGoogleMapButton extends React.Component {
 
-    openMaps(pharmacyAddress, pharmacyName) {
-        const ADDRESS_LENGTH_FOR_ROAD = 9
-        const addressToRoad = pharmacyAddress.substring(0, ADDRESS_LENGTH_FOR_ROAD)
-        window.open(`https://www.google.com.tw/maps/search/${addressToRoad + pharmacyName}`)
-    }
+  openMaps(pharmacyAddress, pharmacyName) {
+    const ADDRESS_LENGTH_FOR_ROAD = 9
+    const addressToRoad = pharmacyAddress.substring(0, ADDRESS_LENGTH_FOR_ROAD)
+    window.open(`https://www.google.com.tw/maps/search/${addressToRoad + pharmacyName}`)
+  }
 
-    render() {
-        const {
-            pharmacyAddress,
-            pharmacyName
-        } = this.props;
+  render() {
+    const { pharmacyAddress, pharmacyName } = this.props
 
-        return (
-            <span
-                onClick={() => { this.openMaps(pharmacyAddress, pharmacyName) }}
-                className="to-google-map-button"
-            >
-                在 Google Maps 打開
-            </span>
-        )
-    }
+    return (
+      <span
+        onClick={() => {
+          this.openMaps(pharmacyAddress, pharmacyName)
+        }}
+        className='to-google-map-button'>
+        在 Google Maps 打開
+      </span>
+    )
+  }
 }
 
-export default ToGoogleMapButton;
+ToGoogleMapButton.propTypes = {
+  pharmacyAddress: PropTypes.string,
+  pharmacyName: PropTypes.string,
+}
+
+export default ToGoogleMapButton
